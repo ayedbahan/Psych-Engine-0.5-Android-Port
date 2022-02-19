ab95378f8239fe79fb5d5053dc67833d05758250
ayedbahan <97188929+ayedbahan@users.noreply.github.com>
2022-02-20 07:03:55 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1155,6 +1155,9 @@ class PlayState extends MusicBeatState
 				case 'senpai' | 'roses' | 'thorns':
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
+ 
+                                case 'splatithon':
+                                       videoIntro(splatithon);
 
 				default:
 					startCountdown();
